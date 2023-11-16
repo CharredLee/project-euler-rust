712855f8eecf5fe32d62c867b6f1510c0a626ea3
Charlie Conneen <gcc2hq@virginia.edu>
2023-11-16 12:20:23 -0500

    solved p7; slightly refactored module creation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(unused)]
 
-mod p1_100;
+pub mod p1_100;
 
 // change the following line to `use p1_100::p?::problem;` to run code 
-use p1_100::p6::problem;
+use p1_100::p7::problem;
 
 
 fn main() {
--- src/p1_100/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod common;
-pub mod p1;
-pub mod p2;
-pub mod p3;
-pub mod p4;
-pub mod p5;
-pub mod p6;
