345fe8d6f17347aa236ce446252b2036b9970141
Charlie Conneen <gcc2hq@virginia.edu>
2023-12-13 04:58:44 -0500

    removed nonsense

--- src/p1_100/p024.rs
+++ src/p1_100/p024.rs
@@ -1,13 +1,7 @@
-use reikna::factor;
-
 use super::common::factorial;
 
-pub fn problem() {
-    // (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-    // (0, 1, 2, 3, 4, 5, 6, 7, 9, 8)
 
-    // Since 9! = 362880, there are 362880 permutations starting with 0,
-    // and also 362880 permutations starting with 1, and so on.
+pub fn problem() {
     let mut target = 1_000_000 - 1;
     let mut digits = (0..10).collect::<Vec<u8>>();
     let mut current_digit = 9;
